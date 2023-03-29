0c60ff61b4ddd36ce67a23b39fc4b53638e1a0f9
yohan <noyohanx@naver.com>
2023-03-29 11:04:19 +0900

    :sparkles: s3 조회 기능, 말따라하기 기능 추가

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from to import Token
+import boto3
 
 bot = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 
@@ -12,7 +13,24 @@ async def on_ready():
     print('------')
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def s3(ctx, *, text):
+    if text == "보여줘":
+        s3 = boto3.resource('s3')
+        bucket_list = []
+
+        for bucket in s3.buckets.all():
+            bucket_list.append(bucket.name)
+
+        await ctx.send(bucket_list)
+    else :
+        await ctx.send("말을 끝까지해줘")
+
+@bot.command()
+async def 따라하기(ctx, *, text):
+    await ctx.send(text)
+
+
+
+
 
 bot.run(Token)
